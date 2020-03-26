65f603ed5cdcfaee31ad7d4cfb4a71445b7fd252
wangxiaobo33 <wangxiaobo9@jd.com>
2020-03-26 14:12:52 +0800

    add element-ui

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'element-ui/lib/theme-chalk/index.css'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
+import ElementUI from 'element-ui'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
   router,
   store,
