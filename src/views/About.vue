8565b2b57f297340697fdd2c01aeb2cd663c6747
wangxiaobo33 <wangxiaobo9@jd.com>
2020-04-09 18:35:50 +0800

    变量使用

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-
-    <el-button>el button</el-button>
+    <p>{{pname}}</p>
+    <el-button @click="changeName">el button</el-button>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data () {
+    return {
+      pname: 'pname'
+    }
+  },
+  methods: {
+    changeName () {
+      this.pname = 'changeName'
+    }
+  }
+
+})
+</script>
