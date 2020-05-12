b4539c0e316950628bf5fb33ed454ebbb35e139d
wangxiaobo33 <wangxiaobo9@jd.com>
2020-05-12 14:18:08 +0800

    jquery

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,15 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  created () {
+    console.log('jquery is ', jQuery)
+    console.log('moment is', moment)
+    console.log(moment().format('YYYY-MM-DD'))
+    console.log('lodash is ', _)
+    console.log('lodash now = ', _.now())
+  }
+})
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,7 @@
-const path = require('path')
+
 const webpack = require('webpack')
 module.exports = {
-    publicPath: './',
-    filenameHashing: true,
     configureWebpack: {
-        devServer: {
-            disableHostCheck: true,
-            // host: 'local.jd.com',
-            port: '80'
-        },
         plugins:[
             new webpack.ProvidePlugin({
                 $:'jquery',
@@ -19,11 +12,5 @@ module.exports = {
             
             })
         ]
-    },
-    chainWebpack: config => {
-        config.plugin('html').tap(args => {
-            args[0].minify = false
-            return args
-        })
     } 
 }
